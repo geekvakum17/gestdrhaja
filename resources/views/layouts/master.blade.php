47d8f0bc9b036b85630deef9045f484c88811af2
geek Phenix <geekphenix35@gmail.com>
2021-02-17 13:02:55 +0000

    ok pour ce que j'ai pu faire

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('layouts.head')
+<body class="">
+@include('layouts.master')
+@include('layouts.sidebar')       
+
+</body>
+@include('layouts.script')
+</html>
