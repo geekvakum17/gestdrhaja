928373dfa609b76449784d34485b74384ca287f2
geek Phenix <geekphenix35@gmail.com>
2021-03-29 07:47:58 +0000

    update day

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,50 +1,48 @@
 @extends('auth.layouts.main')
 @section('content')
-<div class="auth-wrapper align-items-stretch aut-bg-img">
-	<div class="flex-grow-1">
-		<div class="h-100 d-md-flex align-items-center auth-side-img">
-			<div class="col-sm-10 auth-content w-auto">
-				<img src="assets/images/auth/aej.png" alt="" class="img-fluid">
-				<h1 class="text-white my-4">Bienvenue dans le logiciel de gestion des congés </h1>
-				<h4 class="text-white font-weight-normal">Connectez-vous à votre compte .</h4>
-			</div>
-		</div>
-		<div class="auth-side-form">
-			<form method="POST" action="{{ route('session.login') }}">
-				@csrf
-				<div class=" auth-content">
-					<img src="assets/images/auth/auth-logo-dark.png" alt="" class="img-fluid mb-4 d-block d-xl-none d-lg-none">
-					<h3 class="mb-4 f-w-400">S'identifier</h3>
-					<div class="form-group mb-3">
-						<label class="floating-label" for="Email">Adresse e-mail</label>
-						<input type="text" class="form-control @error('email') is-invalid @enderror" name="email" id="Email" value="{{ old('email') }}" placeholder="" required autocomplete="email" autofocus>
-						@error('email')
-						<span class="invalid-feedback" role="alert">
-							<strong>{{ $message }}</strong>
-						</span>
-						@enderror
-					</div>
-					<div class="form-group mb-4">
-						<label class="floating-label" for="Password">Mot de passe</label>
-						<input type="password" class="form-control @error('password') is-invalid @enderror" id="Password" name="password" required autocomplete="current-password">
 
-						@error('password')
-						<span class="invalid-feedback" role="alert">
-							<strong>{{ $message }}</strong>
-						</span>
-						@enderror
-					</div>
-					<!-- <div class="custom-control custom-checkbox text-left mb-4 mt-2">
-						<input type="checkbox" class="custom-control-input" id="customCheck1">
-						<label class="custom-control-label" for="customCheck1">Enregistrer les identifiants.</label>
-					</div> -->
-					<button type="submit" class="btn btn-primary"> S'identifier </button>
 
-					<!-- <div class="text-center">
-						<p class="mb-0 text-muted">Vous n’avez pas de compte? <a href="auth-signup-img-side.html" class="f-w-400">S'inscrire</a></p>
-					</div> -->
+<!-- [ auth-signup ] start -->
+<div class="auth-wrapper">
+	<div class="auth-content">
+		<div class="card">
+			<div class="row align-items-center text-center">
+
+				<div class="col-md-12">
+					<div class="card-body">
+						<img src="assets/images/auth/aej.png" alt="" class="img-fluid mb-4">
+						<h4 class="mb-3 f-w-400">Bienvenue</h4>
+						<h4 class="mb-3 f-w-400">Connectez-vous</h4>
+						<form method="POST" action="{{ route('session.login') }}">
+							@csrf
+
+							<div class="form-group mb-3">
+								<label class="floating-label" for="Email">Adresse e-mail</label>
+								<input type="text" class=" form-control @error('email') is-invalid @enderror" name="email" id="Email" value="{{ old('email') }}" placeholder="" required autocomplete="email" autofocus>
+								@error('email')
+								<span class="invalid-feedback" role="alert">
+									<strong>{{ $message }}</strong>
+								</span>
+								@enderror
+							</div>
+							<div class="form-group mb-4">
+								<label class="floating-label" for="Password">Password</label>
+								<input type="password" class="form-control @error('password') is-invalid @enderror" id="Password" name="password" required autocomplete="current-password">
+								@error('password')
+								<span class="invalid-feedback" role="alert">
+									<strong>{{ $message }}</strong>
+								</span>
+								@enderror
+							</div>
+
+							<button type="submit" class="btn btn-primary"> S'identifier </button>
+
+					</div>
 				</div>
+			</div>
 		</div>
 	</div>
 </div>
 @stop
+<!-- [ auth-signup ] end -->
+
