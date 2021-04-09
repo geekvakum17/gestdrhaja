1abecea326c9dbcf74c0b3294cab4e333de431c9
geek Phenix <geekphenix35@gmail.com>
2021-04-09 05:07:13 +0000

    new update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,14 +5,16 @@
 <!-- [ auth-signup ] start -->
 <div class="auth-wrapper">
 	<div class="auth-content">
+           
 		<div class="card">
 			<div class="row align-items-center text-center">
 
 				<div class="col-md-12">
 					<div class="card-body">
 						<img src="assets/images/auth/aej.png" alt="" class="img-fluid mb-4">
-						<h4 class="mb-3 f-w-400">Bienvenue</h4>
-						<h4 class="mb-3 f-w-400">Connectez-vous</h4>
+						<h3>Bienvenu dans</h3>
+						<h3>GESDRHAJA </h3><br>
+						<h4 class="mb-3 f-w-400">Identifiez-vous</h4>
 						<form method="POST" action="{{ route('session.login') }}">
 							@csrf
 
@@ -36,7 +38,7 @@
 							</div>
 
 							<button type="submit" class="btn btn-primary"> S'identifier </button>
-
+						</form>
 					</div>
 				</div>
 			</div>
