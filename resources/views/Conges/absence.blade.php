85f8040b4cdf46c48c8a0edcfce476990a459d72
geek Phenix <geekphenix35@gmail.com>
2021-06-14 08:04:29 +0000

    update3

--- resources/views/Conges/absence.blade.php
+++ resources/views/Conges/absence.blade.php
@@ -5,6 +5,7 @@
  <div class="main-panel">
     <div class="content-wrapper">
       <div class="row">
+
         <div class="col-md-6 grid-margin stretch-card">
           <div class="card">
             <div class="card-body">
@@ -12,136 +13,99 @@
               <p class="card-description">
                 Basic form layout
               </p>
-              <form class="forms-sample">
+              <form class="forms-sample" method="POST" action="{{ route('absence.store') }}">
+                @csrf()
                 <div class="form-group">
-                  <label for="exampleInputUsername1">Username</label>
-                  <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Username">
+                  <label for="exampleInputUsername1">Nom et Prénom de l'Intérim</label>
+                  <input type="text" class="form-control" id="nomiterim" name="nomiterim" placeholder="Username">
                 </div>
+
+                <div class="form-group row">
+                    <label class="col-sm-3 col-form-label">Objet de l'Absence</label><br><br><br><br><br><br>
+
+                <div class="form-check">
+                    <label class="form-check-label">
+                      <input type="radio" class="form-check-input" id="objetabsence" name="objetabsence" value="Mariage" onclick="choice()" checked>
+                      Mariage
+                    </label>
+                </div>&nbsp;&nbsp;
+
+                <div class="form-check">
+                    <label class="form-check-label">
+                      <input type="radio" class="form-check-input" id="objetabsence" name="objetabsence" value="Naissance" onclick="choice()">
+                      Naissance
+                    </label>
+                </div>&nbsp;&nbsp;
+
+                <div class="form-check">
+                    <label class="form-check-label">
+                      <input type="radio" class="form-check-input" id="objetabsence" name="objetabsence" value="Décès" onclick="choice()">
+                      Décès
+                    </label>
+                </div>&nbsp;&nbsp;
+
+                <div class="form-check">
+                    <label class="form-check-label">
+                      <input type="radio" class="form-check-input" id="objetabsence3" name="objetabsence" onclick="choice()">
+                      Autres
+                    </label>
+                    <input type="text" class="form-control" id="objetabsence4" name="objetabsence" style="display: none;">
+                </div>
+
+                </div>
+
+                <script>
+                    function choice() {
+
+                        if ($('input[type=radio][id=objetabsence3]').is(':checked')) {
+                            $('#objetabsence4').css("display", "block");
+                        } else if ($('input[type=radio][id=objetabsence]').is(':checked')) {
+                            $('#objetabsence4').css("display", "none");
+                        }
+                    }
+                </script>
+
                 <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                    <label for="exampleInputUsername1">DATE DE DEPART</label>
+                    <input type="text" class="form-control" id="datedepart" name="datedepart" placeholder="">
                 </div>
+
                 <div class="form-group">
-                  <label for="exampleInputPassword1">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <label for="exampleInputUsername1">DATE DE RETOUR PREVUE</label>
+                    <input type="text" class="form-control" id="dateretour" name="dateretour" placeholder="">
                 </div>
+
                 <div class="form-group">
-                  <label for="exampleInputConfirmPassword1">Confirm Password</label>
-                  <input type="password" class="form-control" id="exampleInputConfirmPassword1" placeholder="Password">
+                    <label for="exampleInputUsername1">Durée en Jours Ouvrables</label>
+                    <input type="text" class="form-control" id="nbrejourouvrable" name="nbrejourouvrable" placeholder="">
                 </div>
-                <div class="form-check form-check-flat form-check-primary">
-                  <label class="form-check-label">
-                    <input type="checkbox" class="form-check-input">
-                    Remember me
-                  </label>
-                </div>
-                <button type="submit" class="btn btn-primary mr-2">Submit</button>
-                <button class="btn btn-light">Cancel</button>
+                <input type="hidden" class="form-control" id="statu" name="statu" value="3">
+
+
+
+                <button type="submit" class="btn btn-success mr-2">Demandez</button>
+                <button class="btn btn-danger">Annuller</button>
               </form>
             </div>
           </div>
         </div>
-        <div class="col-md-6 grid-margin stretch-card">
-          <div class="card">
-            <div class="card-body">
-              <h4 class="card-title">Horizontal Form</h4>
-              <p class="card-description">
-                Horizontal form layout
-              </p>
-              <form class="forms-sample">
-                <div class="form-group row">
-                  <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Email</label>
-                  <div class="col-sm-9">
-                    <input type="text" class="form-control" id="exampleInputUsername2" placeholder="Username">
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Email</label>
-                  <div class="col-sm-9">
-                    <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email">
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <label for="exampleInputMobile" class="col-sm-3 col-form-label">Mobile</label>
-                  <div class="col-sm-9">
-                    <input type="text" class="form-control" id="exampleInputMobile" placeholder="Mobile number">
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <label for="exampleInputPassword2" class="col-sm-3 col-form-label">Password</label>
-                  <div class="col-sm-9">
-                    <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <label for="exampleInputConfirmPassword2" class="col-sm-3 col-form-label">Re Password</label>
-                  <div class="col-sm-9">
-                    <input type="password" class="form-control" id="exampleInputConfirmPassword2" placeholder="Password">
-                  </div>
-                </div>
-                <div class="form-check form-check-flat form-check-primary">
-                  <label class="form-check-label">
-                    <input type="checkbox" class="form-check-input">
-                    Remember me
-                  </label>
-                </div>
-                <button type="submit" class="btn btn-primary mr-2">Submit</button>
-                <button class="btn btn-light">Cancel</button>
-              </form>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 grid-margin stretch-card">
-          <div class="card">
-            <div class="card-body">
-              <h4 class="card-title">Basic form elements</h4>
-              <p class="card-description">
-                Basic form elements
-              </p>
-              <form class="forms-sample">
-                <div class="form-group">
-                  <label for="exampleInputName1">Name</label>
-                  <input type="text" class="form-control" id="exampleInputName1" placeholder="Name">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputEmail3">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Email">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputPassword4">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword4" placeholder="Password">
-                </div>
-                <div class="form-group">
-                  <label for="exampleSelectGender">Gender</label>
-                    <select class="form-control" id="exampleSelectGender">
-                      <option>Male</option>
-                      <option>Female</option>
-                    </select>
-                  </div>
-                <div class="form-group">
-                  <label>File upload</label>
-                  <input type="file" name="img[]" class="file-upload-default">
-                  <div class="input-group col-xs-12">
-                    <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
-                    <span class="input-group-append">
-                      <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
-                    </span>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputCity1">City</label>
-                  <input type="text" class="form-control" id="exampleInputCity1" placeholder="Location">
-                </div>
-                <div class="form-group">
-                  <label for="exampleTextarea1">Textarea</label>
-                  <textarea class="form-control" id="exampleTextarea1" rows="4"></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary mr-2">Submit</button>
-                <button class="btn btn-light">Cancel</button>
-              </form>
-            </div>
-          </div>
-        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         <div class="col-md-6 grid-margin stretch-card">
           <div class="card">
             <div class="card-body">
@@ -677,30 +641,7 @@
             </div>
           </div>
         </div>
-        <div class="col-md-6 grid-margin stretch-card">
-          <div class="card">
-            <div class="card-body">
-              <h4 class="card-title">Typeahead</h4>
-              <p class="card-description">
-                A simple suggestion engine
-              </p>
-              <div class="form-group row">
-                <div class="col">
-                  <label>Basic</label>
-                  <div id="the-basics">
-                    <input class="typeahead" type="text" placeholder="States of USA">
-                  </div>
-                </div>
-                <div class="col">
-                  <label>Bloodhound</label>
-                  <div id="bloodhound">
-                    <input class="typeahead" type="text" placeholder="States of USA">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+
       </div>
     </div>
     <!-- content-wrapper ends -->
