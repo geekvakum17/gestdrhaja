dfc97036cc8c3b64f674d76f74dd3578a3d9776d
geek Phenix <geekphenix35@gmail.com>
2021-06-21 13:58:16 +0000

    update12

---
+++ resources/views/recherche/search.blade.php
@@ -0,0 +1,12 @@
+
+<form method="GET" action="{{-- {{route('user.search')}} --}}">
+    <div class="input-group">
+      <input type="text" class="form-control" name="recherche" placeholder="search" aria-label="search" aria-describedby="search">
+      <div class="input-group-prepend">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <button type="submit" class="btn btn-success btn-icon-text" data-toggle="dropdown" id="profileDropdown6">
+            <span class="input-group-text" id="search"> <i class="mdi mdi-magnify"></i> </span>
+         </button>
+      </div>
+    </div>
+</form>
+
